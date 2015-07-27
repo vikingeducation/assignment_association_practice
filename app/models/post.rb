d82df31961d65265265c08e56b6925a1cc1d52bf
alokp <alokpra@gmail.com>
2015-07-27 16:00:31 -0700

    complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  has_many :comments
+  has_many :comments, :dependent => :destroy
   has_many :post_authorings, :class_name => "UserPosting"
   has_many :authors, source: :user, through: :post_authorings
   has_many :post_taggings
