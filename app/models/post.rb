e7ba4862375a9dfe15d8f27ecb4264dd0be4e180
ayva <olga.ayva@gmail.com>
2015-07-27 15:54:13 -0700

    finished checking

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ActiveRecord::Base
-  has_many :comments
-  has_many :post_authorings, :class_name => "Authorship"
-  has_many :post_taggings, :class_name => "Tagging"
-  has_many :tags, :through => :post_taggings
+  has_many :comments,  :dependent => :destroy
+  has_many :post_authorings, :class_name => "Authorship", :dependent => :nullify
+  has_many :post_taggings, :class_name => "Tagging", :dependent => :nullify
+  has_many :tags, :through => :post_taggings, :dependent => :nullify
 
-  has_many :authors, :through => :post_authorings, source: :user
+  has_many :authors, :through => :post_authorings, source: :user , :dependent => :nullify
 end
