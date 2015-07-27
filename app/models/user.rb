4be26ba21a438fbdb5dc2af35fb062b17fb0c9ec
alokp <alokpra@gmail.com>
2015-07-27 14:33:25 -0700

    working on renaming assoc

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :comments
+  has_many :authored_comments, :class_name => "Comment"
   has_many :user_postings
   has_many :posts, through: :user_postings
-
 
 end
