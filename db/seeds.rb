22058c87b12b2068d0c5d1fcc7316b67fee56df4
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-07-27 13:58:20 -0500

    push for lunch

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.destroy_all
+Comment.destroy_all
+Post.destroy_all
+Tag.destroy_all
+PostTagging.destroy_all
+UserPosting.destroy_all
+
 # Create Users
 
 20.times do |n|
@@ -14,7 +21,7 @@
   	new_post = u.posts.create(:title => "Post#{i}", :body => "Lorem Ipsum...")     #creating 5 posts per user
   	# u.posts << new_post
   		2.times do |f|
-  			new_post.comments.create(:body => "Hi! I'm comment #{f} on post #{i}")   #creating 2 comments per post
+  			new_post.comments.create(:body => "Hi! I'm comment #{f} on post #{i}", :user_id => u.id)   #creating 2 comments per post
   			if f%2 == 0     #creating 1 tag per post
   				new_post.tags.create(:name => "Lorem")
   			else
