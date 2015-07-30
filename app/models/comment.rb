1891afa2c9e95b9d54aef408203db306f96e5977
ayva <olga.ayva@gmail.com>
2015-07-30 14:16:16 -0700

    minor

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ActiveRecord::Base
   belongs_to :author, :class_name => "User", :foreign_key => :user_id
   belongs_to :parent_post, :class_name => "Post", :foreign_key => :post_id
 
-  #validates :parent_post , :presence => true, ?except [:new]
+  #validates :parent_post , :presence => true, unless Proc.new {render :new}
 end
