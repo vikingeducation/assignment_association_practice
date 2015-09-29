65e0fcda5b5301102bd09cecc4e55387d4b2c01e
Chris Scavello <bideowego@gmail.com>
2015-09-29 09:18:38 -0400

    Add user_id to comment migration

--- db/migrate/20150929130259_create_comments.rb
+++ db/migrate/20150929130259_create_comments.rb
@@ -3,8 +3,10 @@ class CreateComments < ActiveRecord::Migration
     create_table :comments do |t|
       t.text :body
       t.integer :post_id
+      t.integer :user_id
 
       t.index :post_id
+      t.index :user_id
 
       t.timestamps null: false
     end
