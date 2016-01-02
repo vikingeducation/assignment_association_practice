8bc38e463aeeeb2263634acd0522519f64e7d40d
Sia <siakaramalegos@gmail.com>
2016-01-02 15:03:58 -0600

    adds more associations

--- associations_blog/app/models/post_author.rb
+++ associations_blog/app/models/post_author.rb
@@ -2,7 +2,7 @@ class PostAuthor < ActiveRecord::Base
   belongs_to :post
 
   # Un-semantically named version
-  # belongs_to :user, foreign_key: :author_id
+  belongs_to :user, foreign_key: :author_id
 
   # Semantically named
   belongs_to :author, class_name: 'User'
--- associations_blog/app/models/user.rb
+++ associations_blog/app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
+  # Many-to-Many regular and renamed
   has_many :post_authors, foreign_key: :author_id
   has_many :authored_posts, through: :post_authors, foreign_key: :author_id, source: :post
-  has_many :comments, foreign_key: :author_id
 
-  # TODO: why doesn't this work?
-  has_many :authored_comments, foreign_key: :author_id, source: :comment
+  # One-to-Many with renamed foreign key and renamed again
+  has_many :comments, foreign_key: :author_id
+  has_many :authored_comments, foreign_key: :author_id, class_name: 'Comment'
+
+  has_many :tags_on_authored_posts, through: :authored_posts, source: :tags
 end
