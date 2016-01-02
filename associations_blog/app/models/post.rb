f829e68f180063457b383ea34c01cc2c27d495ee
Sia <siakaramalegos@gmail.com>
2016-01-02 15:28:08 -0600

    Adds dependent actions for destroy and nullify

--- associations_blog/app/models/post.rb
+++ associations_blog/app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :category
-  has_many :post_authors
+  has_many :post_authors, dependent: :destroy
 
   # Un-renamed version
   # has_many :users, through: :post_authors, foreign_key: :author_id
@@ -8,8 +8,8 @@ class Post < ActiveRecord::Base
   # Semantically named version
   has_many :authors, through: :post_authors, source: :user
 
-  has_many :post_tags
+  has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
