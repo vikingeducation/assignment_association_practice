10faea1957fe79fa828261e6499ac8a4f412323b
Sia <siakaramalegos@gmail.com>
2016-01-05 12:13:39 -0600

    basic post cru methods and pages"

--- associations_blog/app/controllers/posts_controller.rb
+++ associations_blog/app/controllers/posts_controller.rb
@@ -9,7 +9,27 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post, notice: "successfully created!"
+    else
+      flash.now[:alert] = "there was a problem"
+      render :new
+    end
+  end
+
   def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: "successfully updated!"
+    else
+      flash.now[:alert] = "there was a problem"
+      render :edit
+    end
   end
 
   def show
@@ -19,5 +39,9 @@ class PostsController < ApplicationController
 
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
