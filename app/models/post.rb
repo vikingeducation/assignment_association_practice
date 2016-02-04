3da92e4a6eab4010b13002b5554565cdb906b642
Julia Herron <julia.herron@gmail.com>
2016-02-04 15:30:22 -0800

    notes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,11 @@
 class Post < ActiveRecord::Base
   belongs_to :category
   has_many :comments, dependent: :destroy
-  # can't do dependent: :destroy here b/c there's no primary key on this join table, BUT dependent: :delete_all works???
+  # can't do "dependent: :destroy" here b/c there's no primary key on this join table, BUT "dependent: :delete_all" works???
+
+  # ActiveRecord::StatementInvalid: PG::SyntaxError: ERROR:  zero-length delimited identifier at or near """"
+  # LINE 1: DELETE FROM "posts_users" WHERE "posts_users"."" = $1
+
   has_many :post_authorings, class_name: "PostsUser"
   # is the extra SELECT checking for lifecycle callback methods?
   has_many :authors, through: :post_authorings, dependent: :destroy
