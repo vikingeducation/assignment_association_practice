973819557e0de5f61c12cdd2016d8fd030d662fe
Thomas Lo <thomasjinlo@gmail.com>
2016-02-04 14:36:03 -0800

    Work on finishing script

--- script.rb
+++ script.rb
@@ -20,17 +20,43 @@ end
 
 def set_comment_to_user(comment, user)
   comment[:author_id] = user.id
-  puts Hirb::Helpers::AutoTable.render(comment)
+  table_render(comment)
 end
 
 def set_post_to_category(post, category)
   post[:category_id] = category.id
-  puts Hirb::Helpers::AutoTable.render(post)
+  table_render(post)
 end
 
 def replace_user_comments(user, new_comments)
   user.comments = new_comments
-  puts Hirb::Helpers::AutoTable.render(user.comments)
+  table_render(user.comments)
+end
+
+def return_comment_author(comment)
+  table_render(comment.author) 
+end
+
+def list_post_comments(post)
+  table_render(post.comments)
+end
+
+def list_comment_post(comment)
+  table_render(comment.post)
+end
+
+def remove_category_post(category)
+  random_post = category.posts.sample
+  category.posts.delete(random_post)
+  table_render(category.posts)
+end
+
+def list_author_post(user)
+  table_render(user.posts)
+end
+
+def list_all_ids(user)
+  table_render(user.post_ids)
 end
 
 def show_comments
