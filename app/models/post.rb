a047aa779c0acca2e4312363687bbc42d468e8e3
Julia Herron <julia.herron@gmail.com>
2016-02-04 15:19:42 -0800

    dependent destroy minus the infinite loop

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ActiveRecord::Base
   belongs_to :category
-  has_many :comments
+  has_many :comments, dependent: :destroy
+  # can't do dependent: :destroy here b/c there's no primary key on this join table, BUT dependent: :delete_all works???
   has_many :post_authorings, class_name: "PostsUser"
-  has_many :authors, through: :post_authorings
+  # is the extra SELECT checking for lifecycle callback methods?
+  has_many :authors, through: :post_authorings, dependent: :destroy
 
   has_many :post_taggings, class_name: "Tagging"
-  has_many :tags, through: :post_taggings
+  has_many :tags, through: :post_taggings, dependent: :destroy
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,6 @@
 class Tag < ActiveRecord::Base
   has_many :post_taggings, class_name: "Tagging"
-  has_many :tagged_posts, through: :post_taggings
+  # can't do dependent: :destroy because they do SELECTs back and forth FOREVER
+  has_many :tagged_posts, through: :post_taggings, dependent: :destroy
   has_many :authors_of_tagged_posts, through: :tagged_posts, source: :authors
 end
