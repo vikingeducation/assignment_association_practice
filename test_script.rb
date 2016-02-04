43bc34f7f66c1b58bd7ceca5513ca256525c1310
Andrew Baik <and.baik@gmail.com>
2016-02-04 17:16:11 -0500

    more scripting done

--- test_script.rb
+++ test_script.rb
@@ -21,7 +21,7 @@ random_user.posts.each { |post| puts "#{post.id} #{post.title}"}
 # JOIN posts ON post_id=posts.id
 # WHERE users.id=9
 
-# 3. 
+# 3.
 c = Comment.all.sample
 puts "\ncomment current User ID: #{c.user_id}\n"
 c.user_id = User.all.sample.id
@@ -34,7 +34,7 @@ puts "\npost current category: #{p.category_id}\n"
 p.category_id = Category.all.sample.id
 puts "\npost new category: #{p.category_id}\n"
 
-#5. 
+#5.
 
 User.first.comments.each { |comment| puts "#{comment.id} #{comment.body[0..20]}"}
 c = [ Comment.all.sample, Comment.all.sample, Comment.all.sample ]
@@ -63,12 +63,29 @@ pp c
 # puts "#{c.id} #{c.body[0..20]} #{c.post_id}"
 pp c.post
 
+#10.
+Category.create(name: "BUBU")
+
+new_post = Post.create(
+  title: "RANDOM TITLE",
+  body: "JDIFOJSIFJSOD",
+  category_id: Category.all.sample.id
+)
+
+category = new_post.category
+puts "\nCATEGORY #{category.name} POSTS:\n"
+
+puts "NUMBER OF POSTS IN BEFORE DELETE, CATEGORY #{category.name}: #{category.posts.count}"
+category.posts.each { |cp| puts "#{cp.id} #{cp.title}"}
+
+new_post.delete
+
+puts "NUMBER OF POSTS AFTER DELETE, CATEGORY #{category.name}: #{category.posts.count}"
+category.reload
+category.posts.each { |cp| puts "#{cp.id} #{cp.title}"}
 
 
+#11.
+User.all.sample.posts.each { |p| puts "#{p.id} #{p.title}"}
 
-
-
-
-
-
-
+#12.
