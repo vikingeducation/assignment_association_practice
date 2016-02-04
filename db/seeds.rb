73757c28fffb660ba00235a988c580be211617d6
Andrew Baik <and.baik@gmail.com>
2016-02-04 13:45:28 -0500

    finished seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+QUANTITY = 10
+
+QUANTITY.times do |it|
+  User.create(name: Faker::Name.name )
+  Category.create(name: Faker::Book.genre )
+end
+
+QUANTITY.times do |it|
+  Post.create(
+    title: Faker::Hipster.sentence(3),
+    body: Faker::Hipster.paragraphs(1),
+    category_id: Category.all.sample )
+  Tag.create(name: Faker::SlackEmoji.activity)
+
+  UserPost.create(user_id: User.all.sample, post_id: Post.last)
+  PostTag.create(post_id: Post.last , tag_id: Tag.last)
+end
