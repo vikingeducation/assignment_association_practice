c5e5e0121c112b6033efc1e5d30ab3579801a9d5
Andrew Baik <and.baik@gmail.com>
2016-02-04 18:34:53 -0500

    finished user to post renaming, need to do join rename table

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ActiveRecord::Base
   belongs_to :category
   has_many :post_tags
   has_many :tags, :through => :post_tags
+
+
   has_many :user_posts
-  has_many :users, :through => :user_posts
+  has_many :authors, :through => :user_posts, foreign_key: :user_id, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :comments
+  has_many :authored_comments, foreign_key: :user_id, class_name: "Comment"
+
   has_many :user_posts
-  has_many :posts, :through => :user_posts
+  has_many :authored_posts, :through => :user_posts, foreign_key: :user_id, source: :post
 end
