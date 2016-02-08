ffa880710951293d3d9492970c376040705cbcb6
Joshua Masland-Sarani <j.masland@gmail.com>
2016-02-08 13:58:23 -0500

    made controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,46 @@
+class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      redirect_to posts_path
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      redirect_to posts_path
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit( :title,
+                                  :body,
+                                  :category_id,
+                                  :tags_id
+    )
+  end
+end
