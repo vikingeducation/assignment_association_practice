d221f8b7e6c79e2a22ef1d8b8c4901143e43e5de
Sam O'Keefe <samok13@gmail.com>
2016-02-08 15:03:50 -0800

    form_for, form_tag dropdowns

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    @category_options = Category.all.map{|category| [category.name, category.id]}
   end
 
   def create
@@ -21,6 +22,7 @@ class PostsController < ApplicationController
 
   def edit
     @post = Post.find(params[:id])
+    @category_options = Category.all.map{|category| [category.name, category.id]}
   end
 
   def update
