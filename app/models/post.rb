b5b639a4d19e667300b1da61d7af9de44d02eb65
Steven Chang <prime_pork@hotmail.com>
2016-03-24 21:04:29 +1000

    all associations have been written into models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  belongs_to :category
+  has_many :comments
+  has_many :tags, :through => :posts_tags
+  has_many :users, :through => :posts_users
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :comments
+  has_many :posts, :through => :posts_users
+
 end
