59c2f5180beb6dc5ac5b50e9c6da71f05381348c
Steven Chang <prime_pork@hotmail.com>
2016-03-26 17:10:14 +1000

    renamed association from Comment to post. Instead of :post, it's now :parent_post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :author, :class_name => "User", :foreign_key => "user_id"
-  belongs_to :post
+  belongs_to :parent_post, :class_name => "Post", :foreign_key => "post_id"
 
 end
+
