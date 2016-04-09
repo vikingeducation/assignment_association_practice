f93641995ac99542d499a9d06fb31759ba29f328
Steven Chang <prime_pork@hotmail.com>
2016-04-09 15:52:02 +1000

    index page is done, as per mockups

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,19 @@
 class PostsController < ApplicationController
 
   def edit
+    @page_header = "Edit Post #{@post.id}: #{@post.name}"
   end
 
   def index
+    @column_headers = ["ID", "Title", "Tags", "Show", "Edit"]
+    @posts = Post.all
   end
 
   def new
   end
 
   def show
+    @page_header = "Post #{@post.id}: #{@post.name} "
   end
 
 end
