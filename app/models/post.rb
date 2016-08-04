442cc608f4e6cd8720c8c2b54c6fdd8426beb13c
Hannah Squier <hannahsquier@gmail.com>
2016-08-04 10:09:00 -0700

    update seed file

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ApplicationRecord
   has_many :post_tags
   has_many :users, through: :user_posts
   has_many :user_posts
+  belongs_to :category
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,21 +9,33 @@
 multiplier = 10
 
 multiplier.times do |n|
-  a = User.create {
-    name: "user#{n}"
-  }
+  user = User.create(
+    name: "user#{n}")
+
+
   (multiplier/2).times do |m|
-  a.create_post {
-    title: "title#{m}",
-    body: "Some long blurb #{m}"
-  }
+    post = user.posts.create(
+      title: "title#{m}",
+      body: "Some long blurb #{m}"
+     )
+
+
+    3.times do |comment_num|
+      post.comments.create(body: "comment body #{comment_num}")
+
+    end
+
+    3.times do |tag_num|
+      post.tags.find_or_create_by(name: "tag #{tag_num}")
+
+    end
+
+    post.update(category: Category.find_or_create_by(name: "category#{[1, 2, 3].sample}"))
+
+
   end
+
 end
 
 puts "users created"
 
-multiplier.times do |n|
-  Post.create {
-
-  }
-end
