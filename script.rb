4db36eb3a334861de4fa3970eaf99cebcd293b4c
Adrian Mui <adrian.mui@gmail.com>
2016-08-04 14:21:34 -0400

    merge

--- script.rb
+++ script.rb
@@ -3,10 +3,10 @@ p User.find(3).comments
 
 puts  "set a comment to belong to a different user"
 u = User.find(1)
+
+u.comments.first.update(:user_id => 2)
 u_id = u.comments.first.user_id
-u.comments.first.user_id = 2
-u.save
-temp = u.comments.first.user_id
-p temp == 2
+p u_id 
+p u_id == "2"
 
 #4 set a post to be in a different category
