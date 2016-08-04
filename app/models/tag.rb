d11237e9965443fc4448e6aecfc85b2cd4376315
essian <jessicampdennis@hotmail.com>
2016-08-05 00:50:50 +0100

    finishes renaming associations

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,6 @@
 class Tag < ApplicationRecord
   has_many :post_taggings
-  has_many :posts, through: :post_taggings
-
+  has_many :tagged_posts, through: :post_taggings, source: :post
+  has_many :post_authorings, through: :tagged_posts
+  has_many :authors_of_tagged_posts, through: :post_authorings, class_name: "User", source: :user
 end
