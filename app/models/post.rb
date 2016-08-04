ea4b10bf0e8d65a086045c40bfb738cde2cf07d7
Alex Lach <alexglach@gmail.com>
2016-08-04 15:01:41 -0400

    onto renaming associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :category
   has_many :comments
-  has_many :user_posts
+  has_many :post_authorings, :class_name => "UserPost"
   has_many :post_tags
-  has_many :users, :through => :user_posts
+  has_many :authors, :through => :post_authorings
   has_many :tags, :through => :post_tags
 end
