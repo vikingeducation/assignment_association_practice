5488a9c047a8e1dc41385b2680821a6f1a3e17fa
jsteenb2 <jonathansteenbergen@gmail.com>
2016-08-04 10:01:57 -0700

    added seed changes

---
+++ association_tests.rb
@@ -0,0 +1,19 @@
+#!/env/ruby
+
+puts "here lie users"
+pp User.all
+
+puts "here lie posts"
+pp Post.all
+
+puts "here lie comments"
+pp Comment.all
+
+puts "here lie tags"
+pp Tag.all
+
+puts "here lie categories"
+pp Category.all
+
+
+puts "Listing "
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Destroying db crap"
+Category.destroy_all
+Comment.destroy_all
+Post.destroy_all
+User.destroy_all
+Tag.destroy_all
+UserPosting.destroy_all
+PostTagging.destroy_all
+
+
+puts "building categories"
+3.times do |i|
+  c = Category.create(name: "fake_category#{i}")
+end
+
+puts "building users"
+3.times do |i|
+  u = User.create(name: "Name#{i}")
+end
+
+puts "building posts"
+5.times do |i|
+  post = Post.create(title: "post#{i}", body: "body crap ##{i}", category_id: Category.all.sample.id)
+end
+
+puts "building comments"
+3.times do |i|
+  c = Comment.create(body: "body comment #{i}", user_id: User.all.sample.id, post_id: Post.all.sample.id)
+end
+
+puts "building tags"
+3.times do |i|
+  t = Tag.create(name: "tag name #{i}")
+end
+
+puts "connect posts and user"
+User.all.each do |user|
+  user.posts << Post.all.sample
+  user.save
+end
+
+puts "connecting tags and posts"
+Post.all.each do |post|
+  post.tags << Tag.all.sample
+  post.save
+end
