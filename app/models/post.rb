cfe6b63c4265808e9a5faaae526b57693460ae8c
Alex Lach <alexglach@gmail.com>
2016-08-04 13:16:27 -0400

    database is seeded

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :category
   has_many :comments
+  has_many :user_posts
+  has_many :post_tags
   has_many :users, :through => :user_posts
   has_many :tags, :through => :post_tags
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :comments
+  has_many :user_posts
   has_many :posts, :through => :user_posts
 end
