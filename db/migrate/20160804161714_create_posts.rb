2eedd299f0790fe5b3d2849b9af7b0e28547dc06
Alex Lach <alexglach@gmail.com>
2016-08-04 12:46:43 -0400

    done with migration

--- db/migrate/20160804161714_create_posts.rb
+++ db/migrate/20160804161714_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.0]
     create_table :posts do |t|
       t.string :title
       t.text :body
-
+      t.references :category, foreign_key: true
       t.timestamps
     end
   end
