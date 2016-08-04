1a98b39a8dc0c5016708af10b08ac17e830519f9
Dylan Lynch <lynchd2@tcnj.edu>
2016-08-04 14:49:29 -0400

    Work on associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   has_many :post_authorings, class_name: "UserPost"
   has_many :authors, through: :post_authorings, source: :user
   has_many :comments
-  has_many :tag_posts
-  has_many :tags, through: :tag_posts
+  has_many :post_taggings, class_name: "TagPost"
+  has_many :tags, through: :post_taggings, source: :tag
   belongs_to :category
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :post_authorings, class_name: "UserPost"
   has_many :authored_posts, through: :post_authorings, source: :post
   has_many :authored_comments, class_name: "Comment"
+  has_many :tags_on_authored_posts, through: :authored_posts, source: :tag
 end
