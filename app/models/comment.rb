567037ab42fc0422f38b7c586a67db47f6d92567
essian <jessicampdennis@hotmail.com>
2016-08-05 01:03:38 +0100

    starts dependency questions

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: "User", optional: true
+  belongs_to :author, class_name: "User", optional: true, dependent: :destroy
   belongs_to :post, class_name: "Post"
 end
