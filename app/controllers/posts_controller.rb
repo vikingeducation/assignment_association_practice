7995d6f9d05de311837eda99f92fdb01b217ee60
Dylan Lynch <lynchd2@tcnj.edu>
2016-08-05 14:04:43 -0400

    Start views and controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,19 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def update
+  end
+
+  def index
+    @posts = Post.all
+  end
 end
