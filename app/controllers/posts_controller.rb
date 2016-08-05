ea64da3e5ca00e785945e8d40c8bb3d1d08313e6
essian <jessicampdennis@hotmail.com>
2016-08-05 18:17:35 +0100

    sets up basic crud

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def show 
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to post_path(@post)
+  end
+
+  def update
+    @post = Post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :tag_ids => [])
+  end
+end
