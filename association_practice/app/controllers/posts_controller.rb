8b155a531b6f937727de67981205e290352135eb
Leo Ahnn <leoahnn@gmail.com>
2016-08-05 13:19:01 -0400

    working on create

--- association_practice/app/controllers/posts_controller.rb
+++ association_practice/app/controllers/posts_controller.rb
@@ -1,2 +1,28 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "Create worked"
+      redirect_to @post
+    else
+      flash[:alert] = "Create didn't work"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+private
+  def post_params
+    params.require(:post).permit(:title, :body, :category_id, :tag_ids => [] )
+  end
 end
