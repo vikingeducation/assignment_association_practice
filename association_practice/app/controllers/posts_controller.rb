f725db8fdc66cc2dd22411db025b29a41249e5ce
Leo Ahnn <leoahnn@gmail.com>
2016-08-05 14:43:32 -0400

    working on nested forms

--- association_practice/app/controllers/posts_controller.rb
+++ association_practice/app/controllers/posts_controller.rb
@@ -35,6 +35,6 @@ class PostsController < ApplicationController
 
 private
   def post_params
-    params.require(:post).permit(:title, :body, :category_id, :tag_ids => [] )
+    params.require(:post).permit(:title, :body, :category_id, :tag_ids => [], :comment_attributes => [:body, :id, :_destroy ])
   end
 end
--- association_practice/app/models/post.rb
+++ association_practice/app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ApplicationRecord
-  has_many :comments, :dependent => :destroy
 
   has_many :post_authorings, class_name: "UsersPost", foreign_key: :post_id, :dependent => :destroy
   has_many :authors, through: :post_authorings, source: :user
@@ -8,4 +7,8 @@ class Post < ApplicationRecord
   has_many :tags, through: :post_taggings
 
   belongs_to :category
+
+  has_many :comments, :dependent => :destroy
+  accepts_nested_attributes_for :comments,
+                                :allow_destroy => true;
 end
