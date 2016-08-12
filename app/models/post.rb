bc5d979ee3ff6c4389991c028255274505ba136b
David J Watts <davidjeremywatts@gmail.com>
2016-08-12 15:18:17 -0700

    finish dependencies

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
 class Post < ApplicationRecord
 
-  has_many :comments
+  has_many :comments, :dependent => :destroy
   belongs_to :category
 
-  has_many :post_taggings, :class_name => "PostTag"
+  has_many :post_taggings, :class_name => "PostTag", :dependent => :delete_all
   has_many :tags, :through => :post_taggings
 
 
-  has_many :user_posts
+  has_many :user_posts, :dependent => :delete_all
   has_many :authors, :through => :user_posts, :source => :user
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :authored_comments, :class_name => "Comment"
+  has_many :authored_comments, :class_name => "Comment", :dependent => :destroy
 
-  has_many :user_posts
+  has_many :user_posts, :dependent => :delete_all
   has_many :authored_posts, :through => :user_posts, :source => :post
 
   has_many :tags_on_authored_posts, :through => :authored_posts, :source => :tags
