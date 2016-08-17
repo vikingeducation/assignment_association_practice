8cb163523d1da89a984e8cddbafca4a0a46e4807
David Watts <davidjeremywatts@gmail.com>
2016-08-17 12:48:57 -0700

    aap with collection forms

--- db/migrate/20160812212213_create_user_posts.rb
+++ db/migrate/20160812212213_create_user_posts.rb
@@ -1,8 +1,8 @@
 class CreateUserPosts < ActiveRecord::Migration[5.0]
   def change
-    create_table :user_posts do |t|
-
-      t.timestamps
-    end
+    # create_table :user_posts do |t|
+    #
+    #   t.timestamps
+    # end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812152456) do
+ActiveRecord::Schema.define(version: 20160812212213) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
