f4f18c94e964048cbdc5dd8e1db54c6023005dd4
Kenneth Kottenstette <kennykotten@gmail.com>
2016-12-01 15:13:58 -0800

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+NAMES = ["Anthony", "Erik", "Alexa", "Catherine", "Dexter", "JP", "Kenny", "James"]
+CATEGORIES = ["fishing", "dancing", "coding"]
+TAGS = ["Tag 1", "Tag 2", "Tag 3"]
+POST_CONTENT = ["Post body 1", "Post body 2", "Post body 3"]
+
+#populates categories table
+CATEGORIES.each do |cat|
+  Category.create(name: cat)
+end
+
+#populates tags table
+TAGS.each do |tag|
+  Tag.create(name: tag)
+end
+
+#creates a user, the user's posts, the post's category
+NAMES.each do |name|
+
+  a = User.create(name: name)
+  b = a.posts.create(title: POST_CONTENT.sample, body: POST_CONTENT.sample)
+  c = b.category = Category.find_by_name(CATEGORIES.sample)
+
+  b.save
+
+  PostTag.create(post: b, tag: Tag.find_by_name(TAGS.sample))
+
+
+end
