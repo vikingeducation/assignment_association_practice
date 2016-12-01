18015486edcda1d47d9d4890114b563877289df7
Jonathan Popenuck <john.popenuck@gmail.com>
2016-12-01 14:41:18 -0800

    finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :authored_posts, :through => :userposts, :source => :post
   has_many :userposts
-  has_many :post_authorings, :through => :userposts, :source => :post
-  has_many :authored_comments, :class_name => "Comment", :foreign_key => :author_id, :dependent => :destroy 
+  has_many :post_authorings, :through => :userposts, :source => :post, :dependent => :destroy
+  has_many :authored_comments, :class_name => "Comment", :foreign_key => :author_id, :dependent => :destroy
   has_many :tags_on_authored_posts, :through => :post_authorings, :source => :tags
 end
