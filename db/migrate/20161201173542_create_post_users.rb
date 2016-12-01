c472b0a20313122a64a161fc05c79eb2ce6f395c
luke-schleicher <luke.j.schleicher@gmail.com>
2016-12-01 11:36:58 -0600

    generated models

---
+++ db/migrate/20161201173542_create_post_users.rb
@@ -0,0 +1,10 @@
+class CreatePostUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :post_users do |t|
+      t.references :user, foreign_key: true
+      t.references :post, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
