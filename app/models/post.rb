09b36e3fede75636aca93035d3c06779639220df
Alexa <alexa.anderson@outlook.com>
2016-12-01 12:43:16 -0600

    Add files

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :category
+  has_many :comments
+  has_many :post_users
+  has_many :users, :through => :post_users
+  has_many :post_tags
+  has_many :tags, :through => :post_tags
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :comments
+  has_many :post_users
+  has_many :posts, through: :post_users
 end
