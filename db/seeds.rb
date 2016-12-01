7211f8b72a24366be0b6cfe85876b44327279cad
Alexa <alexa.anderson@outlook.com>
2016-12-01 12:04:03 -0600

    Begin seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# TODO: Refactor to use .all instead of arrays
+
+NUM_USERS = 20
+NUM_POSTS = 100
+NUM_CATEGORIES = 5
+NUM_TAGS = 3
+NUM_COMMENTS = 200
+
+
+## Create Users
+users = []
+NUM_USERS.times do |i|
+  users << User.create(name: Faker::Name.name)
+end
+
+## Create Categories
+categories = []
+NUM_CATEGORIES.times do |i|
+  categories << Category.create(name: Faker::Pokemon.name)
+end
+
+## Create Posts
+posts = []
+NUM_POSTS.times do |i|
+  posts << Post.create({title: Faker::ChuckNorris.fact,
+                        body: Faker::Lorem.paragraph,
+                        category_id: categories.sample.id})
+end
+
+## Create Comments
+NUM_COMMENTS.times do |i|
+  Comment.create({ body: Faker::Lorem.paragraph,
+                   user_id: users.sample.id,
+                   post_id: posts.sample.id })
+end
+
+## Create Tags
+NUM_TAGS.times do |i|
+  Tag.create(name: Faker::Music.instrument)
+end
+
+
+####### Many-to-Many
