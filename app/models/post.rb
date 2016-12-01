e227fbdbc05aec9430f87f3dd4669f39ede4ed4b
Alexa <alexa.anderson@outlook.com>
2016-12-01 17:34:46 -0600

    Add some associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,16 @@
 class Post < ApplicationRecord
   belongs_to :category
   has_many :comments
-  has_many :post_users
-  has_many :users, :through => :post_users
+
+  has_many :post_authorings,
+           :class_name => "PostUser",
+           :foreign_key => :post_id
+
+  has_many :authors,
+           :through => :post_authorings,
+           :foreign_key => :post_id,
+           :source => :author # Points to what join table belongs to
+
   has_many :post_tags
   has_many :tags, :through => :post_tags
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
-  has_many :comments
-  has_many :post_users
-  has_many :posts, through: :post_users
+  has_many :authored_comments,
+           :class_name => "Comment", :foreign_key => :user_id
+
+  has_many :post_authorings,
+           :class_name => "PostUser",
+           :foreign_key => :user_id
+  
+  has_many :authored_posts,
+           :through => :post_authorings,
+           :foreign_key => :user_id,
+           :source => :post
 end
