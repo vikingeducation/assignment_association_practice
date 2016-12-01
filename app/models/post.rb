2cc24acaf3d1d402f3578f73f560a426e8175e01
James Harris <computersherpa@gmail.com>
2016-12-01 12:48:20 -0600

    Association between Users and Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ApplicationRecord
     has_many :comments
     has_many :users, :foreign_key => :author_id
+
     has_many :post_tags
     has_many :tags, :through => :post_tags
+
+    has_many :user_posts, foreign_key: :post_id
+    has_many :authors, through: :user_posts, source: :user
 end
