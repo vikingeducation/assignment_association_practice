bf0fcff12df16a38e2af754f402ab160d9d7c656
James Harris <computersherpa@gmail.com>
2016-12-01 19:00:44 -0600

    More work on tests

--- tests.rb
+++ tests.rb
@@ -12,19 +12,24 @@ puts Hirb::Helpers::AutoTable.render Comment.all
 random_comment = Comment.all.sample
 puts "Lucky comment of the day is ##{random_comment.id}. Former author: #{random_comment.user.name}."
 random_comment.user = User.all.sample
+random_comment.save
 puts "New author: #{random_comment.user.name}."
 
 random_post = Post.all.sample
-puts "Lucky comment of the day is ##{random_post.id}. Old category: #{random_post.category.name}."
+puts "Lucky post of the day is ##{random_post.id}. Old category: #{random_post.category.name}."
 random_post.category = Category.all.sample
 puts "New category: #{random_post.category.name}."
+puts "Post's comments:"
+puts Hirb::Helpers::AutoTable.render random_post.comments
 
 random_user = User.all.sample
 puts "Comments of user #{random_user.name}:"
 puts Hirb::Helpers::AutoTable.render random_user.comments
 puts "New comments of user:"
 random_user.comments = [Comment.first, Comment.second]
+random_user.save
+
 puts Hirb::Helpers::AutoTable.render random_user.comments
 
 random_comment = Comment.all.sample
-puts "Lucky comment of the day is ##{random_comment.id}. Its author is #{random_comment.user}."
+puts "Lucky comment of the day is ##{random_comment.id}. Its author is #{random_comment.user.name}. Its parent post is ##{random_comment.post.id}."
