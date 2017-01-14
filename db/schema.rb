a314047977c85774da097b1cf1822d973d0c181f
tt <ttwang.303@gmail.com>
2017-01-14 18:45:20 -0500

    seed database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170114233517) do
+ActiveRecord::Schema.define(version: 20170114234422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20170114233517) do
 
   create_table "post_tags", force: :cascade do |t|
     t.integer  "post_id"
-    t.integer  "user_id"
+    t.integer  "tag_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
