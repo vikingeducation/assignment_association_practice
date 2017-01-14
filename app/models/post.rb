004c1b9d395e2d0c603ece35f5b6d6e240de7e6c
tt <ttwang.303@gmail.com>
2017-01-14 18:50:27 -0500

    add basic associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+  has_many :comments
+
+  has_many :user_posts
+  has_many :users, through: :user_posts
+
+  has_many :post_tags
+  has_many :tags, through: :post_tags
 end
--- app/models/post_tag.rb
+++ app/models/post_tag.rb
@@ -1,2 +1,4 @@
 class PostTag < ApplicationRecord
+  belongs_to :tag
+  belongs_to :post
 end
