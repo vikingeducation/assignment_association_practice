54b710c8a3028f39f5ac2c1bba6e40cc43e6164b
tt <ttwang.303@gmail.com>
2017-01-14 21:28:10 -0500

    rename associations and add dependencies

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
-  has_many :user_posts
-  has_many :users, through: :user_posts
+  has_many :post_authorings, class_name: 'UserPost', dependent: :destroy
+  has_many :authors, through: :post_authorings, source: :user
 
-  has_many :post_tags
-  has_many :tags, through: :post_tags
+  has_many :post_taggings, class_name: 'PostTag', dependent: :destroy
+  has_many :tags, through: :post_taggings, class_name: 'PostTag', source: :tag
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  has_many :comments
+  has_many :authored_comments, class_name: 'Comment', dependent: :destroy
 
-  has_many :user_posts
-  has_many :posts, through: :user_posts
+  has_many :post_authorings, class_name: 'UserPost', dependent: :destroy
+  has_many :authored_posts, through: :post_authorings, source: :post
+
+  has_many :post_taggings, through: :authored_posts, source: :post_taggings
+  has_many :tags_on_authored_posts, through: :post_taggings, source: :tag
 end
