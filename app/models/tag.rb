ea561ddc22e1c1aaf26e439fccbb4d300976b00f
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-03-01 12:07:25 +0800

    Manage dependents

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
-  has_many :post_taggings
+  has_many :post_taggings, :dependent => :destroy
   has_many :tagged_posts, :through => :post_taggings, source: :post
   has_many :user_postings, :through => :tagged_posts
   has_many :authors_of_tagged_posts, :through => :user_postings, source: :user
