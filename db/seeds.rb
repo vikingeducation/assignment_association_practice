9f687f64f754ddc8351c56b5d715122f080f22de
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-03-01 11:34:17 +0800

    Make records easier to compare

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+PostTagging.destroy_all
+UserPosting.destroy_all
+Comment.destroy_all
+Post.destroy_all
+User.destroy_all
+Category.destroy_all
+Tag.destroy_all
+
+
+10.times do |i|
+  Category.create!(name: "#{Faker::Book.genre}")
+  Tag.create!(name: "#{Faker::Book.publisher}")
+end
 
 10.times do |i|
   i += 1
-  User.create!(name: "user#{i}")
-  Category.create!(name: "Category #{i}")
   2.times do
     j = rand(1..10)
-    Post.create!(title: "#{j}: Lorem ipsum dolor sit amet.", body: "#{j} Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia nisi nihil iste beatae esse dignissimos quam facere, consequuntur explicabo amet.", category_id: "#{j}")
+    Post.create!(title: "#{Faker::Book.title}: Lorem ipsum dolor sit amet.", body: "#{j} Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia nisi nihil iste beatae esse dignissimos quam facere, consequuntur explicabo amet.", category_id: "#{Category.pluck(:id).sample}")
   end
-  Comment.create!(body: "#{i}: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, quos.", user_id: "#{i}", post_id: "#{i}")
-  Tag.create!(name: "#{i} Lorem.")
-  PostTagging.create!(post_id: "#{i}", tag_id: "#{i}")
+  User.create!(name: "user#{i}")
+  Comment.create!(body: "#{Faker::Book.title}", user_id: "#{i}", post_id: "#{i}")
+  PostTagging.create!(post_id: "#{i}", tag_id: "#{Tag.pluck(:id).sample}")
   UserPosting.create!(user_id: User.pluck(:id).sample, post_id: Post.pluck(:id).sample)
 end
