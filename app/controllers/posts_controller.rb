7169ea1c9cff1875e0efa7e28e68b2c667b00e39
Anthony Sin <anthony.t.sin@gmail.com>
2017-03-09 23:38:56 -0500

    nested forms practice

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,15 +3,19 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  # https://www.amooma.de/screencasts/2015-01-22-nested_forms-rails-4.2/
   def new 
     @categories = Category.all
     @tags = Tag.all
     @users = User.all
     @post = Post.new
+    @post.comments.build
+    @post.comments.build
   end
 
   def create
-    @post = Post.create(post_params)
+    @post = Post.create!(post_params)
+    # fail
     # fail
     # http://stackoverflow.com/questions/403671/the-differences-between-build-create-and-create-and-when-should-they-be-us
     redirect_to post_path(@post.id)
@@ -22,6 +26,7 @@ class PostsController < ApplicationController
     @tags = Tag.all
     @users = User.all
     @post = Post.find(params[:id])
+    @post.comments.build
   end
 
   # http://api.rubyonrails.org/classes/ActiveRecord/Persistence.html#method-i-update
@@ -35,6 +40,8 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post)
-      .permit(:title, :body, :category_id, tag_ids: [], user_ids: [])
+      .permit(:title, :body, :category_id, tag_ids: [], user_ids: [],
+          comments_attributes: [ :body, :user_id ]
+        )
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :category
 
-  has_many :comments, dependent: :destroy
+  has_many :comments, dependent: :destroy, inverse_of: :post
   has_many :postings, dependent: :destroy
   has_many :users, through: :postings
   has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
+
+  accepts_nested_attributes_for :comments
 end
