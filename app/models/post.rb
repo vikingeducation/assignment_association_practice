21489e967be3fb9a832ff2a39eb55f4d8ba2d3a3
Bideo Wego <bideowego@gmail.com>
2017-05-03 05:05:10 -0400

    Update

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,23 @@
 class Post < ActiveRecord::Base
   belongs_to :category
-  has_many :comments, :dependent => :destroy
-  has_many :post_authorings, :class_name => 'UserPosting', :dependent => :destroy
-  has_many :authors, :through => :post_authorings, :class_name => 'User', :source => :user
-  has_many :post_taggings, :dependent => :destroy
-  has_many :tags, :through => :post_taggings
+  
+  has_many  :comments,
+            :dependent => :destroy
+  
+  has_many  :post_authorings,
+            :class_name => 'UserPosting',
+            :dependent => :destroy
+  
+  has_many  :authors,
+            :through => :post_authorings,
+            :class_name => 'User',
+            :source => :user
+  
+  has_many  :post_taggings,
+            :dependent => :destroy
+  
+  has_many  :tags,
+            :through => :post_taggings
 
   accepts_nested_attributes_for :comments,
                                 :reject_if => :all_blank,
