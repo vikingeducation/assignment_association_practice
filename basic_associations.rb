bea214ef9fd53aa85cac0924d14feb00f64e07c5
efraimmgon <efraimmaugusto@gmail.com>
2017-05-14 12:35:30 -0400

    refactor basic_associations to reflect new names

--- basic_associations.rb
+++ basic_associations.rb
@@ -6,59 +6,59 @@ Category.all
 Tag.all
 
 # List a given user's comments
-User.first.comments
+User.first.authored_comments
 
 # Set a comment to belong to a different user
-User.first.comments << Comment.all.sample
+User.first.authored_comments << Comment.all.sample
 
 # Set a post to be in a different category
 Post.first.category = Category.all.sample
 
 # Set a collection of comments to replace a user's current comments (e.g.
 # User.first.comments = [Comment.first, Comment.second])
-User.first.comments = [Comment.first, Comment.second]
+User.first.authored_comments = [Comment.first, Comment.second]
 
 # Return a given comment's author
-Comment.first.user
+Comment.first.author
 
 # List a given post's comments
 Post.first.comments
 
 # Return a given comment's parent post
-Comment.first.post
+Comment.first.parent_post
 
 # Remove one post from a category's collection of posts
 Category.first.posts.destroy(Category.first.posts.first)
 
 # List the posts authored by a given user
-User.first.posts
+User.first.authored_posts
 
 # List the IDs of all posts authored by a given user (hint: there's an
 # association method for this)
-User.first.post_ids
+User.first.authored_post_ids
 
 # Set a collection of Posts to replace that user's currently authored posts,
 # e.g. User.first.posts = [Post.first, Post.second]
-User.first.posts = Post.all[0..10]
+User.first.authored_posts = Post.all[0..10]
 
 # List the authors of a given post
-Post.first.users
+Post.first.authors
 
 # Set a collection of Users to replace a given Post's authors in a similar way
-Post.first.users = User.all[0..3]
+Post.first.authors = User.all[0..3]
 
 # Accomplish the same thing by only using IDs (hint: there's an association
 # method for this...)
-Post.first.user_ids = User.ids[0..1]
+Post.first.author_ids = User.ids[0..1]
 
 # List the posts under a given tag
-Tag.first.posts
+Tag.first.post_taggings
 
 # Add a new post to a given tag by only using its ID
-Tag.first.post_ids << Post.first.id
+Tag.first.post_tagging_ids << Post.first.id
 
 # Add a new tag to a given post by only using its ID
-Post.first.tags << Tag.first.id
+Post.first.post_taggings << Tag.first.id
 
 # List the tags on a given post
-Post.first.tags
+Post.first.post_taggings
