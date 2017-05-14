f58885dfc83b241f9a1324a0eb65d46adb84f6ec
efraimmgon <efraimmaugusto@gmail.com>
2017-05-14 11:57:39 -0400

    modify relation between user<>comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :user
+  belongs_to :author,
+             :foreign_key => :user_id,
+             :class_name => "User"
   belongs_to :post
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-  has_many :comments
+  #has_many :comments
+  has_many :authored_comments,
+           :foreign_key => :user_id,
+           :class_name => "Comment"
   has_many :user_posts
   has_many :posts,
            :through => :user_posts,
