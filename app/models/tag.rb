1ff8d4a90a6e90ed17eea5f06c397b7be4d91cb0
efraimmgon <efraimmaugusto@gmail.com>
2017-05-14 12:10:30 -0400

    rename association between tag>post

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,7 @@
 class Tag < ApplicationRecord
   has_many :post_tags
-  has_many :posts,
+  has_many :tagged_posts,
            :through => :post_tags,
+           :source => :post,
            :dependent => :destroy
 end
