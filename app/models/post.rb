6ae5a583cd9d2cb4d7805559f87c9fd3d198f164
efraimmgon <efraimmaugusto@gmail.com>
2017-05-30 18:09:45 -0400

    practice with collections forms

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_many :comments
-  belongs_to :category
+  #belongs_to :category
   has_many :user_posts
   has_many :authors,
            :through => :user_posts,
