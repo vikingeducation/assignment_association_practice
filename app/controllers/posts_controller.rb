128b8aad7ef00992bd961fdf942858f2ff90c2cd
Ann Allan <ann.cristina.allan@gmail.com>
2017-07-09 01:30:56 -0400

    Add solution to Practice

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,40 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @tag_options = Tag.all.map{|u| [u.name, u.id]}
+    @category_options = Category.all.map{|u| [u.name, u.id]}
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(safe_post_params)
+    if @post.save
+      flash[:success] = "New post created"
+      redirect_to 'show'
+    else
+      flash[:error] = "Post creation failed"
+      render 'new'
+    end
+  end
+
+  def edit
+    @post  = Post.find(params[:id])
+  end
+
+  def update
+    if @post.update_attributes(safe_post_params)
+      flash[:success] = "Post updated"
+      redirect_to 'show'
+    else
+      flash[:error] = "Post update failed"
+      render 'edit'
+    end
+  end
+
 end
