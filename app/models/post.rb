1c5688a8b80561ba138c727eaf7b97c705ba2640
Roy Chen <roychenlei@gmail.com>
2017-08-29 14:12:06 +0800

    added dependent management to models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :category
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   has_many :post_authorings, class_name: "UserPosting"
-  has_many :authors, through: :post_authorings, source: :user
+  has_many :authors, through: :post_authorings, source: :user, dependent: :nullify
 
   has_many :post_taggings, class_name: "PostTagging"
-  has_many :tags, through: :post_taggings
+  has_many :tags, through: :post_taggings, dependent: :nullify
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
   has_many :post_taggings, class_name: "PostTagging"
-  has_many :tagged_posts, through: :post_taggings, source: :post
+  has_many :tagged_posts, through: :post_taggings, source: :post, dependent: :nullify
 
   has_many :authors_of_tagged_posts, through: :tagged_posts, source: :authors
 end
