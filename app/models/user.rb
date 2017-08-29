60cfe80c70eeef28ae8be69f450b11a41cc6ca8d
Chad Lucas <chad.lucas.19@gmail.com>
2017-08-29 16:36:52 -0700

    add post_taggings model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
 	has_many :posts, through: :post_users, dependent: :destroy
 	has_many :comments, foreign_key: :user_id
 	
-	belongs_to :comments, optional: true
+	
 end
