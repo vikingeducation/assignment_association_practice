e0ee9158eac3c541ed54733f0fb3f1c7c0e4e118
Roy Chen <roychenlei@gmail.com>
2017-08-29 09:39:03 +0800

    made changes to test file to accommodate renamed associations

--- test.rb
+++ test.rb
@@ -10,23 +10,21 @@ end
 # List a given user's comments
 def list_comments(user)
   puts "Listing Comments for User.."
-  p user.comments
+  p user.authored_comments
   puts
 end
 
 # Set a comment to belong to a different user
 def set_comment_to_different_user
   comment = Comment.all.sample
+  puts "Comment #{comment.id} currently belongs to Author #{comment.author.id}."
 
-  puts "Comment #{comment.id} currently belongs to User #{comment.user.id}."
-
-  user = comment.user
-  user = User.all.sample until user != comment.user
-
-  comment.user = user
+  author = comment.author
+  author = User.all.sample until author != comment.author
+  comment.author = author
   comment.save!
 
-  puts "Comment #{comment.id} now belongs to User #{comment.user.id}."
+  puts "Comment #{comment.id} now belongs to Author #{comment.author.id}."
 end
 
 # Set a post to be in a different category
@@ -49,17 +47,17 @@ def replace_user_comments(num_comments = 5)
   random_comments = Comment.all.sample(num_comments)
 
   user = User.all.sample
-  puts "User #{user.id} currently has the Comments: #{user.comments.pluck(:id)}"
+  puts "User #{user.id} currently has authored the Comments: #{user.authored_comments.pluck(:id)}"
 
-  user.comments = random_comments
-  puts "User #{user.id} now has the Comments: #{user.comments.pluck(:id)}"
+  user.authored_comments = random_comments
+  puts "User #{user.id} now has authored the Comments: #{user.authored_  comments.pluck(:id)}"
 end
 
 # Return a given comment's author
 def get_comment_author(comment)
-  if comment.user
-    puts "Comment #{comment.id} has author #{comment.user.id}."
-    return comment.user
+  if comment.author
+    puts "Comment #{comment.id} has author #{comment.author.id}."
+    return comment.author
   else
     puts "Comment #{comment.id} has no author (you may want to check why!)"
   end
@@ -77,9 +75,9 @@ end
 
 # Return a given comment's parent post
 def get_comment_parent(comment)
-  if comment.post
-    puts "Comment #{comment.id}'s parent Post is #{comment.post.id}."
-    comment.post
+  if comment.parent_post
+    puts "Comment #{comment.id}'s parent Post is #{comment.parent_post.id}."
+    comment.parent_post
   else
     puts "Comment #{comment.id} has no parent Post (you may want to check why!)"
   end
@@ -99,17 +97,17 @@ end
 def list_posts_authored_by(user)
   puts "User #{user.id} has the following posts: "
 
-  user.posts.each do |post|
-    puts "ID: #{post.id}"
-    puts "Title: #{post.title}"
-    puts "Body: #{post.body}"
+  user.authored_posts.each do |authored_post|
+    puts "ID: #{authored_post.id}"
+    puts "Title: #{authored_post.title}"
+    puts "Body: #{authored_post.body}"
   end
 end
 
 # List the IDs of all posts authored by a given user (hint: there's an association method for this)
 def list_posts_id_authored_by(user)
   puts "User #{user.id} authored the following Post IDs:"
-  p user.posts.pluck(:id)
+  p user.authored_posts.pluck(:id)
 end
 
 # Set a collection of Posts to replace that user's currently authored posts, e.g. User.first.posts = [Post.first, Post.second]
@@ -117,16 +115,16 @@ def replace_user_posts(num_posts = 5)
   random_posts = Post.all.sample(num_posts)
 
   user = User.all.sample
-  puts "User #{user.id} currently has the Posts: #{user.posts.pluck(:id)}"
+  puts "User #{user.id} currently has the Posts: #{user.authored_posts.pluck(:id)}"
 
-  user.posts = random_posts
-  puts "User #{user.id} now has the Posts: #{user.posts.pluck(:id)}"
+  user.authored_posts = random_posts
+  puts "User #{user.id} now has the Posts: #{user.authored_posts.pluck(:id)}"
 end
 
 # List the authors of a given post
 def list_post_authors(post)
   puts "Post #{post.id} has these authors:"
-  post.users.each do |author|
+  post.authors.each do |author|
     puts "Name: #{author.name}"
   end
 end
@@ -137,7 +135,7 @@ def replace_post_authors(num_authors = 3)
   list_post_authors(post)
 
   random_authors = User.all.sample(num_authors)
-  post.users = random_authors
+  post.authors = random_authors
   list_post_authors(post)
 end
 
@@ -147,15 +145,15 @@ def replace_post_authors_by_id(num_authors = 3)
   list_post_authors(post)
 
   random_author_ids = User.all.pluck(:id).sample(num_authors)
-  post.user_ids = random_author_ids
+  post.author_ids = random_author_ids
   list_post_authors(post)
 end
 
 # List the posts under a given tag
 def list_posts_under_tag(tag)
-  if tag.posts
+  if tag.tagged_posts
     puts "Tag #{tag.id} has these Posts:"
-    tag.posts.each do |post|
+    tag.tagged_posts.each do |post|
       puts "ID: #{post.id}"
       puts "Title: #{post.title}"
       puts "Body: #{post.body}"
@@ -179,7 +177,7 @@ end
 # (it looks like I have to do this by adding a new PostTagging)
 def add_post_to_tag_by_id(post_title, post_body, tag)
   puts "Tag #{tag.id} has these Posts: "
-  p tag.posts.pluck(:title)
+  p tag.tagged_posts.pluck(:title)
 
   post = Post.new(title: post_title, body: post_body)
   post.category_id = Category.pluck(:id).sample
@@ -191,7 +189,7 @@ def add_post_to_tag_by_id(post_title, post_body, tag)
   end
 
   puts "Tag #{tag.id} now has these Posts: "
-  p tag.posts.pluck(:title)
+  p tag.tagged_posts.pluck(:title)
 end
 
 # Add a new tag to a given post by only using its ID
