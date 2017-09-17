6ce4734455d043763dd09afdce33b36c5855cadb
Chad Lucas <chad.lucas.19@gmail.com>
2017-09-17 10:31:49 -0700

    rename user to author association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,12 @@
 class Post < ApplicationRecord
 	
-	has_many :users, through: :post_users, dependent: :destroy
+	has_many :authors, through: :post_users, dependent: :destroy, :source => :users
 	
 	has_many :categories
 	has_many :comments, dependent: :destroy
 	
-	has_many :post_taggings, dependent: :destroy
 	has_many :tags, through: :post_taggings
+    has_many :post_taggings, dependent: :destroy
+	
 
 end
