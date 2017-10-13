ab137c831a21f72d64550cd62ddbf81a4e5ee72a
Sean Luckett <whitecrane007@gmail.com>
2017-10-13 15:41:53 -0600

    Arrange for sane data deletion

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :category
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
-  has_many :post_authorings
+  has_many :post_authorings, dependent: :destroy
   has_many :users, through: :post_authorings
 
-  has_many :post_taggings
+  has_many :post_taggings, dependent: :destroy
   has_many :tags, through: :post_taggings
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :authored_comments, class_name: 'Comment'
+  has_many :authored_comments, class_name: 'Comment', dependent: :destroy
 
-  has_many :post_authorings
+  has_many :post_authorings, dependent: :destroy
   has_many :authored_posts, through: :post_authorings, source: :post
 
   has_many :post_taggings, through: :authored_posts
