4a4acd355103e5e6d8ac39d499321ae7acce9c58
Sean Luckett <whitecrane007@gmail.com>
2017-10-13 11:43:39 -0600

    Derp: adds overlooked, missing associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :comments
 
   has_many :user_posts
-  has_many :posts, through: :user_posts
+  has_many :users, through: :user_posts
 
   has_many :post_tags
   has_many :tags, through: :post_tags
