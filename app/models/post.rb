d3303364f3f65423968b14c150d92964a62b0533
Jacob Wald <schoen575@gmail.com>
2017-11-27 12:21:52 -0500

    add seed data

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :category
+
+  has_many :user_posts
+  has_many :users, through: :user_posts
+
+  has_many :comments
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,52 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning Database"
+DatabaseCleaner.clean_with :truncation
+
+# users
+puts "Creating Users"
+
+7.times do
+  User.create!(name: Faker::Name.name )
+end
+# categories
+
+puts "Creating Catogories"
+
+10.times do
+  Category.create!(name: Faker::Lorem.word)
+end
+
+# tags
+
+puts "Creating Tags"
+6.times do
+  Tag.create!(name: Faker::Lorem.word)
+end
+
+# association
+
+# User X:X Posts, Category 1:X Post
+
+puts "Creating Posts"
+User.all.each do |user|
+  (rand(1..5)).times do
+    user.posts.create! do |p|
+      p.title = Faker::Lorem.sentence
+      p.body = Faker::Lorem.paragraph
+      p.category = Category.find(Category.pluck(:id).sample)
+    end
+  end
+end
+# User 1:X Comments, Post 1:X Comments
+
+puts "Adding Comments to Some Posts"
+7.times do
+  post = Post.find(Post.pluck(:id).sample)
+
+  post.comments.create!(body: Faker::Lorem.paragraph, user_id: rand(1..7))
+end
+# Post X:X Tags
+
+puts "Adding Tags to Some Posts"
+6.times do |n|
+  PostTagging.create!(post_id: n + 1, tag_id: rand(1..6))
+end
