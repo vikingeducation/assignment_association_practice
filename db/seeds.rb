c64c375cc829f104e58db8c153008b52724706a1
Sean Kelly <shonkelli@gmail.com>
2017-11-30 21:31:22 -0500

    added foreign keys

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+array = Array(0..5)
+array.each do |num|
+  User.create(:name => "user" + num.to_s)
+end
