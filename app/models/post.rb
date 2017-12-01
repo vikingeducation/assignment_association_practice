9234c4e4fffe1a1f3d0c0d1f5883e946d832c740
Sean Kelly <shonkelli@gmail.com>
2017-11-30 23:23:37 -0500

    added seed files

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ApplicationRecord
   belongs_to :category
   has_many :user_posts
   has_many :post_tags
-  has_many :posts, :through => :user_posts
+  has_many :users, :through => :user_posts
   has_many :tags, :through => :post_tags
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,34 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
 Category.destroy_all
+Post.destroy_all
+Tag.destroy_all
+Comment.destroy_all
+PostTags.destroy_all
+UserPosts.destroy_all
 
-array = Array(0..5)
+array = Array(0..10)
 array.each do |num|
   User.create(:name => "user" + num.to_s)
+end
+
+array.each do |num|
+  Category.create(:name => "category" + num.to_s)
+end
+
+array.each do |num|
+  Post.create(:title => "Post Name " + num.to_s, :body => "This is the text and stuff", :category_id => Category.all.sample.id)
+end
+
+array.each do |num|
+  Tag.create(:name => "#" + num.to_s)
+end
+
+array.each do |num|
+  Comment.create(:body => "I think we need #{num} seals", :user_id => User.all.sample.id, :post_id => Post.all.sample.id)
+end
+
+array.each do |num|
+  PostTags.create(:post_id => Post.all.sample.id, :tag_id => Tag.all.sample.id)
+  UserPosts.create(:post_id => Post.all.sample.id, :user_id => User.all.sample.id)
 end
